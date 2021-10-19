11786e97e14b389c50e4ccea9d5c1f01f948020c
Mohosin Islam Palash <mohosinislampolash@gmail.com>
2021-10-19 19:57:16 +0600

    Blog Done

--- src/Pages/Blog/Blog/Blog.js
+++ src/Pages/Blog/Blog/Blog.js
@@ -1,9 +1,22 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import blogs from '../../../AllData/blogs.json'
+import SingleBlog from '../../Shared/SingleBlog/SingleBlog';
 const Blog = () => {
+    const [blog, setBlog] = useState([]);
+
+    useEffect(() => {
+        setBlog(blogs);
+        console.log(blogs);
+    }, [blog]);
+
     return (
         <div>
-            <h1>This is blog home</h1>
+            <h1 className="heading">NEWS & EVENTS</h1>
+            <div className="blog-container">
+                {
+                    blog.map(blog => <SingleBlog blog={blog}></SingleBlog>)
+                }
+            </div>
         </div>
     );
 };
